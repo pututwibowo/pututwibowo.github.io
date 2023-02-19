6f976f3c10e205e8a761da19e98ad6ba99917924
pututwibowo <putut.info@gmail.com>
2023-02-19 16:25:54 +0700

    menambahkan project NFT

---
+++ project/NFT1/script.js
@@ -0,0 +1,10 @@
+const faqSlideSwitch=document.querySelectorAll('.faq-slide-switch')
+const faqSlide=document.querySelectorAll('.faq-slide')
+
+
+for (let i=0;i<faqSlideSwitch.length;i++){
+	faqSlideSwitch[i].addEventListener('click',
+	function (){
+		faqSlide[i].classList.toggle('faq-slide-active')
+	})
+}
